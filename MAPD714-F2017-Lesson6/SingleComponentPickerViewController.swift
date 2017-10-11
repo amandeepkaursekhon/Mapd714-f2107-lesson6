eb395f15cee3fc2a57d9f62993b030b2e94ba109
amandeepkaursekhon <amandeepsekhon2017@gmail.com>
2017-10-11 19:21:42 -0400

    first commit

---
+++ MAPD714-F2017-Lesson6/SingleComponentPickerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SingleComponentPickerViewController.swift
+//  MAPD714-F2017-Lesson6
+//
+//  Created by Amandeep Sekhon on 2017-10-11.
+//  Copyright © 2017 Amandeep Sekhon. All rights reserved.
+//
+
+import UIKit
+
+class SingleComponentPickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
